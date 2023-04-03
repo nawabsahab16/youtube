a4c0c30037ada3b37cc930ea82abd247983fb48f
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-04-03 15:55:18 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { AppContext } from "./context/contextApi";
+const App = () => {
+  return (
+    <AppContext>
+       <div className="text-3xl"> App </div>
+    </AppContext>
+  )
+}
+
+export default App
